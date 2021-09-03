6b097b64c9f0f6b5e687593cf087e76ac245d162
dmitry andreev <djandreev89@mail.ru>
2021-09-03 15:02:16 +1000

    update webpack.config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+class Example {
+  constructor(element) {
+    this.element = element;
+  }
+
+  init() {
+    this.element.textContent = '!!!hello, world!';
+    console.log('ehu!');
+  }
+}
+
+const a = () => {
+  const element = document.querySelector('.point');
+  const obj = new Example(element);
+  obj.init();
+};
+
+a();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+// const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -13,8 +13,8 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
